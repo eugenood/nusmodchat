5fd0ca0483e6ce3f3de481989589721b5d350cc7
E-Liang Tan <eliang@eliangtan.com>
2019-04-10 19:36:29 +0800

    Add and run prettify script
    Use Prettier to make code formatting consistent.

--- public/client.js
+++ public/client.js
@@ -1,28 +1,38 @@
-$(function () {
-  
-  $.get('/entries', function (entries) {
-    entries.forEach(function (entry) {
-      $('<li></li>').html(convertEntryToDisplay(entry)).appendTo('ul#entries');
+$(function() {
+  $.get('/entries', function(entries) {
+    entries.forEach(function(entry) {
+      $('<li></li>')
+        .html(convertEntryToDisplay(entry))
+        .appendTo('ul#entries');
     });
   });
 
-  $('form').submit(function (event) {
+  $('form').submit(function(event) {
     event.preventDefault();
-    var moduleCode = $('input#moduleCode').val().toUpperCase();
+    var moduleCode = $('input#moduleCode')
+      .val()
+      .toUpperCase();
     var chatUrl = $('input#chatUrl').val();
     if (moduleCode !== '') {
       var newEntry = { moduleCode: moduleCode, chatUrl: chatUrl };
       $.post('/entries?' + $.param(newEntry), function() {
-        $('<li></li>').html(convertEntryToDisplay(newEntry)).appendTo('ul#entries');
+        $('<li></li>')
+          .html(convertEntryToDisplay(newEntry))
+          .appendTo('ul#entries');
         $('input#moduleCode').val('');
         $('input#chatUrl').val('');
         $('input#moduleCode').focus();
       });
     }
   });
-  
 });
 
 function convertEntryToDisplay(entry) {
-  return '<a href="' + entry.chatUrl + '" target="_blank">' + entry.moduleCode + '</a>';
+  return (
+    '<a href="' +
+    entry.chatUrl +
+    '" target="_blank">' +
+    entry.moduleCode +
+    '</a>'
+  );
 }
